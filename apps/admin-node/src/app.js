73a90c75c69ac6fa44bb55651e66e88a7d4884ce
Shasta Ged-ang <2232833@slu.edu.ph>
2025-12-16 00:23:46 +0800

    updated index.ejs and layout.ejs

--- apps/admin-node/src/app.js
+++ apps/admin-node/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
-app.set('layout', 'layouts/main');
+app.set('layout', 'layouts/layout');
 
 
 app.use(express.static(path.join(__dirname, '../public')));
