db6fe583ceac3c0df54c9df6ceab9b7d17791f85
gavrelleGarcia <2244653@slu.edu.ph>
2025-12-16 00:13:17 +0800

    remove the error.ejs and index.ejs inside the view. it will now automatically redirectes to the dashboard when trying to run npm start

--- apps/admin-node/src/routes/index.js
+++ apps/admin-node/src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // Home page
 router.get('/', (req, res) => {
-    res.render('index', {
+    res.render('dashboard/index', {
         title: 'SLUBazaar Admin Panel'
     });
 });
